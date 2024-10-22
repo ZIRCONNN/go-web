4c9662866b84e8a3adc932b5cd6525be8947fa39
ZIRCONNN <81862999+ZIRCONNN@users.noreply.github.com>
2024-10-22 16:28:34 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Goodbye, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/goodbye", goodbyeHandler)
+
+	fmt.Println("Server is listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
